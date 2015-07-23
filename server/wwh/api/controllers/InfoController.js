1501a12ec761ff9a830f7f6630e0dc40ddc291d3
Allen Koo <crossgate9@gmail.com>
2015-07-24 00:36:57 +0800

    [add] list info

--- server/wwh/api/controllers/InfoController.js
+++ server/wwh/api/controllers/InfoController.js
@@ -6,10 +6,19 @@ module.exports = {
     Info.create({
       data: data
     }).exec(function(err, info) {
-      console.log(err);
-      console.log(info);
-
       return res.json(info);
     });
-  }
+  },
+
+  list: function(req, res) {
+    Info.find().exec(function(err, info) {
+      return res.json(info);
+    })
+  },
+
+  // reset: function(req, res) {
+  //   Info.drop(function(err) {
+  //     return res.json('success');
+  //   });
+  // }
 };
